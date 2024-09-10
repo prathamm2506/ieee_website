23f71b15372a8645237823f7606c0bb6cd560494
prathamm2506 <fs20if005@gmail.com>
2024-09-10 11:22:15 +0530

    index.css, folder structure, and basic variables defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>This is heading 1</h1>
+      <h2>This is heading 2</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit aspernatur corrupti totam, libero velit ad deleniti nesciunt facere ducimus consequuntur distinctio obcaecati animi soluta? Temporibus, dolor. Cumque, tempora molestiae. Error tenetur atque natus recusandae optio, ipsam aliquid ratione quia hic.</p>
+      <small>This is small text</small>
+      <Navbar/>
     </>
   )
 }
